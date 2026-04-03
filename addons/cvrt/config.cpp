2ac6c1eafb2544d7ac63844edc534b612a888a6c
George Dennington <georgedennington501@gmail.com>
2026-04-03 23:11:36 +0100

    Add RARDEN status functionality to CVRT addon
    - Introduced a new self-action for RARDEN status in the CVRT vehicles, allowing players to check the loaded and available ammunition.
    - Implemented conditions and exceptions for the action to ensure it is only available when the player is in the correct position.
    - Enhanced user feedback with formatted hints displaying ammunition status.

--- addons/cvrt/config.cpp
+++ addons/cvrt/config.cpp
@@ -620,6 +620,20 @@ class CfgVehicles
 		};
 		class ACE_SelfActions: ACE_SelfActions
 		{
+			class RARDENStatus
+			{
+				icon="\20ABCT\data\UI\RARDEN.paa";
+				displayName="RARDEN Status";
+				condition="player == (vehicle player) turretUnit [0,0]";
+				exceptions[]=
+				{
+					"isNotInside",
+					"isNotSitting"
+				};
+				statement="call {private _veh = vehicle player; private _tMags = _veh magazinesTurret [0]; private _loadedAPDS = {_x == '20ABCT_3Rnd_30mm_APDS'} count _tMags; private _loadedHEIT = {_x == '20ABCT_3Rnd_30mm_HEIT'} count _tMags; private _pItems = items player; private _cItems = (getItemCargo _veh) select 0; private _availAPDS = ({_x == '20ABCT_Item_APDS_Clip'} count _pItems) + ({_x == '20ABCT_Item_APDS_Clip'} count _cItems); private _availHEIT = ({_x == '20ABCT_Item_HEIT_Clip'} count _pItems) + ({_x == '20ABCT_Item_HEIT_Clip'} count _cItems); private _lines = []; {_x params ['_m','_t','_a']; if (_m == '20ABCT_3Rnd_30mm_APDS') then {_lines pushBack format ['  SABOT: %1/3 rds', _a]}; if (_m == '20ABCT_3Rnd_30mm_HEIT') then {_lines pushBack format ['  SHELL: %1/3 rds', _a]}} forEach magazinesAllTurrets _veh; hint parseText format ['<t size=''1.1'' font=''PuristaBold''>RARDEN L21A2</t><br/><br/><t font=''PuristaMedium''>LOADED (%1/2):</t><br/>%2<br/><br/><t font=''PuristaMedium''>AVAILABLE:</t><br/>  SABOT clips: %3<br/>  SHELL clips: %4', _loadedAPDS + _loadedHEIT, if (count _lines == 0) then {'  Empty'} else {_lines joinString '<br/>'}, _availAPDS, _availHEIT]}";
+				showDisabled=0;
+				priority=10;
+			};
 			class LoadRARDEN
 			{
 				icon="\20ABCT\data\UI\RARDEN.paa";
@@ -1371,6 +1385,20 @@ class CfgVehicles
 		};
 		class ACE_SelfActions: ACE_SelfActions
 		{
+			class RARDENStatus
+			{
+				icon="\20ABCT\data\UI\RARDEN.paa";
+				displayName="RARDEN Status";
+				condition="player == (vehicle player) turretUnit [0,0]";
+				exceptions[]=
+				{
+					"isNotInside",
+					"isNotSitting"
+				};
+				statement="call {private _veh = vehicle player; private _tMags = _veh magazinesTurret [0]; private _loadedAPDS = {_x == '20ABCT_3Rnd_30mm_APDS'} count _tMags; private _loadedHEIT = {_x == '20ABCT_3Rnd_30mm_HEIT'} count _tMags; private _pItems = items player; private _cItems = (getItemCargo _veh) select 0; private _availAPDS = ({_x == '20ABCT_Item_APDS_Clip'} count _pItems) + ({_x == '20ABCT_Item_APDS_Clip'} count _cItems); private _availHEIT = ({_x == '20ABCT_Item_HEIT_Clip'} count _pItems) + ({_x == '20ABCT_Item_HEIT_Clip'} count _cItems); private _lines = []; {_x params ['_m','_t','_a']; if (_m == '20ABCT_3Rnd_30mm_APDS') then {_lines pushBack format ['  SABOT: %1/3 rds', _a]}; if (_m == '20ABCT_3Rnd_30mm_HEIT') then {_lines pushBack format ['  SHELL: %1/3 rds', _a]}} forEach magazinesAllTurrets _veh; hint parseText format ['<t size=''1.1'' font=''PuristaBold''>RARDEN L21A2</t><br/><br/><t font=''PuristaMedium''>LOADED (%1/2):</t><br/>%2<br/><br/><t font=''PuristaMedium''>AVAILABLE:</t><br/>  SABOT clips: %3<br/>  SHELL clips: %4', _loadedAPDS + _loadedHEIT, if (count _lines == 0) then {'  Empty'} else {_lines joinString '<br/>'}, _availAPDS, _availHEIT]}";
+				showDisabled=0;
+				priority=10;
+			};
 			class LoadRARDEN
 			{
 				icon="\20ABCT\data\UI\RARDEN.paa";
